76664be896a45a4f6dd7d3a34c0d62bdcb82c567
SAHILKHAN0207 <sabbakhan1805@gmail.com>
2026-06-29 14:23:16 +0530

    idk what i am fixing

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,4 +1,3 @@
-
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
@@ -9,14 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-
-
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+console.log('Cloudinary config:', process.env.CLOUDINARY_CLOUD_NAME);
 
 const storage = new CloudinaryStorage({
   cloudinary,
